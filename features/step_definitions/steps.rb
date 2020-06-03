289de4c19462cf9a079124e415559435d53125f3
Joao Bizeli <you@example.com>
2020-06-03 16:42:03 -0300

    exec

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,16 @@
+class Restaurante
+    def prato_do_dia(dia)
+      case (dia)
+      when "segunda-feira"
+        return "Virado a Paulista"
+      when "terça-feira"
+        return "Dobradinha"
+      when "quarta-feira"
+        return "Feijoada"
+      when "quinta-feira"
+        return "Macarrão"
+      when "sexta-feira"
+        return "Filé de Peixe"
+      end
+    end
+  end
